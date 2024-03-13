efc797392253917cc5691675c30bc3dd7288582a
Albert J. Wong <awong.dev@gmail.com>
2024-03-12 22:50:06 -0700

    basic custom dict file support

--- lib/jieba.ex
+++ lib/jieba.ex
@@ -10,11 +10,14 @@ defmodule Jieba do
 
       iex> Jieba.split("呢個係乜嘢呀")
 
+      iex> Jieba.split_custom("呢個係乜嘢呀", "/path/to/dictfile")
   """
   use Rustler,
     otp_app: :jieba, # must match the name of the project in `mix.exs`
     crate: :rustler_jieba # must match the name of the crate in `native/jieba/Cargo.toml`
 
-  def split(_arg1), do: :erlang.nif_error(:nif_not_loaded)
+  def split(_text), do: :erlang.nif_error(:nif_not_loaded)
+
+  def split_custom(_text, _dict_file), do: :erlang.nif_error(:nif_not_loaded)
 
 end
--- native/rustler_jieba/src/lib.rs
+++ native/rustler_jieba/src/lib.rs
@@ -1,9 +1,26 @@
+use std::fs::File;
+use std::io::BufReader;
+
 use jieba_rs::Jieba;
 
 #[rustler::nif]
-fn split(txt: String) -> Vec<String> {
+fn split(text: String) -> Vec<String> {
     let jieba = Jieba::new();
-    return jieba.cut(&txt, true).into_iter().map(|s| s.to_string()).collect();
+    return jieba.cut(&text, true).into_iter().map(|s| s.to_string()).collect();
+}
+
+#[rustler::nif]
+fn split_custom(text: String, dict_path: String) -> Vec<String> {
+    return match File::open(dict_path) {
+      Ok(f) => {
+        let mut reader = BufReader::new(f);
+        return match Jieba::with_dict(&mut reader) {
+          Ok(jieba) => jieba.cut(&text, true).into_iter().map(|s| s.to_string()).collect(),
+          Err(err) => vec!(err.to_string())
+        };
+      },
+      Err(err) => vec!(err.to_string())
+    };
 }
 
 rustler::init!("Elixir.Jieba", [split]);
