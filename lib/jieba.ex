d2ebd59e3d7c5a7ae15c7b92cb58b08ba2f7205e
Albert J. Wong <awong.dev@gmail.com>
2024-03-12 23:11:50 -0700

    Add example runs showing custom dict.
    Example from original jieba project showing segmentation change.

--- lib/jieba.ex
+++ lib/jieba.ex
@@ -9,8 +9,15 @@ defmodule Jieba do
   ## Examples
 
       iex> Jieba.split("呢個係乜嘢呀")
+      ["呢", "個", "係", "乜嘢", "呀"]
 
-      iex> Jieba.split_custom("呢個係乜嘢呀", "/path/to/dictfile")
+      iex> Jieba.split("李小福是创新办任也是云计算方面的家")
+      ["李小福", "是", "创新", "办任", "也", "是", "云", "计算",
+       "方面", "的", "家"]
+
+      iex> Jieba.split_custom("李小福是创新办任也是云计算方面的家", "example_userdict.txt")
+      ["李小福", "是", "创新办", "任", "也", "是", "云", "计算",
+       "方面", "的", "家"]
   """
   use Rustler,
     otp_app: :jieba, # must match the name of the project in `mix.exs`
