087b1e5c122f23f0afb2af59d06252ee75229ffb
Albert J. Wong <awong.dev@gmail.com>
2024-03-13 20:55:07 -0700

    Refactor to keep jieba struct alive and propogate errors

--- lib/jieba.ex
+++ lib/jieba.ex
@@ -8,14 +8,19 @@ defmodule Jieba do
 
   ## Examples
 
-      iex> Jieba.split("呢個係乜嘢呀")
+      iex> j = Jieba.make()
+
+      iex> Jieba.split(j, "呢個係乜嘢呀")
       ["呢", "個", "係", "乜嘢", "呀"]
 
-      iex> Jieba.split("李小福是创新办任也是云计算方面的家")
+      iex> Jieba.split(j, "李小福是创新办任也是云计算方面的家")
       ["李小福", "是", "创新", "办任", "也", "是", "云", "计算",
        "方面", "的", "家"]
 
-      iex> Jieba.split_custom("李小福是创新办任也是云计算方面的家", "example_userdict.txt")
+      iex> Jieba.load_dict(j, "example_userdict.txt")
+      :ok
+
+      iex> Jieba.split(j, "李小福是创新办任也是云计算方面的家")
       ["李小福", "是", "创新办", "任", "也", "是", "云", "计算",
        "方面", "的", "家"]
   """
@@ -23,8 +28,8 @@ defmodule Jieba do
     otp_app: :jieba, # must match the name of the project in `mix.exs`
     crate: :rustler_jieba # must match the name of the crate in `native/jieba/Cargo.toml`
 
-  def split(_arg1), do: :erlang.nif_error(:nif_not_loaded)
+  def make(), do: :erlang.nif_error(:nif_not_loaded)
+  def load_dict(_jieba, _dict_path), do: :erlang.nif_error(:nif_not_loaded)
 
-  def split_custom(_arg1, _arg2), do: :erlang.nif_error(:nif_not_loaded)
-
+  def split(_jieba, _text), do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/rustler_jieba/src/lib.rs
+++ native/rustler_jieba/src/lib.rs
@@ -1,27 +1,76 @@
+use jieba_rs::{Jieba, Error as JiebaError};
+use rustler::{Atom, Env, Error as RustlerError, ResourceArc, Term};
 use std::fs::File;
 use std::io::BufReader;
+use std::sync::Mutex;
+use std::io::Error as IoError;
+use std::io::ErrorKind as IoErrorKind;
 
-use jieba_rs::Jieba;
+// Creates an atoms module using the rustler macro
+mod atoms {
+    rustler::atoms! {
+        ok,
+        invalidentry,
 
-#[rustler::nif]
-fn split(text: String) -> Vec<String> {
-    let jieba = Jieba::new();
-    return jieba.cut(&text, true).into_iter().map(|s| s.to_string()).collect();
+        // Posix
+        enoent, // File does not exist
+        eacces, // Permission denied
+        epipe, // Broken pipe
+        eexist, // File exists
+
+        io_unknown // Other error
+    }
+}
+
+pub struct JiebaResource {
+    jieba: Mutex<Jieba>,
+}
+
+fn on_load(env: Env, _term: Term) -> bool {
+    rustler::resource!(JiebaResource, env);
+    true
+}
+
+// Translates std library errors into Rustler atoms
+fn io_error_to_term(err: &IoError) -> Atom {
+    match err.kind() {
+        IoErrorKind::NotFound => atoms::enoent(),
+        IoErrorKind::PermissionDenied => atoms::eacces(),
+        IoErrorKind::BrokenPipe => atoms::epipe(),
+        IoErrorKind::AlreadyExists => atoms::eexist(),
+        _ => atoms::io_unknown(),
+    }
 }
 
 #[rustler::nif]
-fn split_custom(text: String, dict_path: String) -> Vec<String> {
-    return match File::open(dict_path) {
-      Ok(f) => {
-        let mut reader = BufReader::new(f);
-        let mut jieba = Jieba::new();
-        return match jieba.load_dict(&mut reader) {
-          Ok(_) => jieba.cut(&text, true).into_iter().map(|s| s.to_string()).collect(),
-          Err(err) => vec!(err.to_string())
-        };
-      },
-      Err(err) => vec!(err.to_string())
-    };
+fn make() -> ResourceArc<JiebaResource> {
+    ResourceArc::new(JiebaResource {
+        jieba: Mutex::new(Jieba::new()),
+    })
 }
 
-rustler::init!("Elixir.Jieba", [split, split_custom]);
+#[rustler::nif]
+fn load_dict(env: Env, resource: ResourceArc<JiebaResource>, dict_path: String) -> Result<Term, RustlerError> {
+    match File::open(dict_path) {
+        Ok(f) => {
+            let jieba = &mut *resource.jieba.lock().unwrap();
+            let mut reader = BufReader::new(f);
+            match jieba.load_dict(&mut reader) {
+                Ok(()) => Ok(atoms::ok().to_term(env)),
+                    Err(jieba_err) => match jieba_err {
+                        JiebaError::Io(ref io_err) => Err(RustlerError::Term(Box::new(io_error_to_term(io_err)))),
+                        JiebaError::InvalidDictEntry(entry) => Err(RustlerError::Term(Box::new(entry)))
+                    }
+            }
+        },
+            Err(ref io_err) => Err(RustlerError::Term(Box::new(io_error_to_term(io_err))))
+    }
+}
+
+#[rustler::nif]
+fn split(resource: ResourceArc<JiebaResource>, text: String) -> Vec<String> {
+    let ref jieba= *resource.jieba.lock().unwrap();
+    jieba.cut(&text, true).into_iter().map(|s| s.to_string()).collect()
+}
+
+rustler::init!("Elixir.Jieba", [make, load_dict, split], load = on_load);
