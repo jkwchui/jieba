cc453986bd1a20233f34e3b24101019f1b949562
Albert J. Wong <awong.dev@gmail.com>
2024-03-25 12:31:49 -0700

    Fix licensing, versioning, packaging

--- mix.exs
+++ mix.exs
@@ -1,20 +1,26 @@
 defmodule Jieba.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/awong-dev/jieba"
+  @version "0.1."
+
   def project do
     [
       app: :jieba,
-      version: "1.0.0",
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      docs: docs()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: []
     ]
   end
 
@@ -22,6 +28,31 @@ defmodule Jieba.MixProject do
   defp deps do
     [
       {:rustler, "~> 0.31.0"}
+    ]
+  end
+
+  defp description() do
+    """
+    Elixir API to Rust Jieba-RS Chinese segmenter.
+    """
+  end
+
+  defp package() do
+    [
+      maintainers: ["Albert J. Wong"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      name: "Jieba",
+      source_ref: "v#{@version}",
+      canonical: "http://hexdocs.pm/jason",
+      source_url: @source_url,
+      extras: ["README.md", "CHANGELOG.md", "LICENSE"]
     ]
   end
 end
